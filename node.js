4b487694325e5d2750633ee250eb281fb3a7ab26
AParAPares <amandapares87@gmail.com>
2023-09-17 00:17:55 -0300

    cambios nuevos

---
+++ node.js
@@ -0,0 +1,16 @@
+//funcion para el bloque con cambio de color mensajes
+function over(element) {
+    alert("mouseover");
+}
+
+function out(element) {
+    alert("mouseout");
+}
+//cambio de color en si
+function over(element) {
+    element.style.backgroundColor = "lime";
+}
+
+function out(element) {
+    element.style.backgroundColor = "silver";
+}
